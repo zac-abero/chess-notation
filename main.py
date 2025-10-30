18a1b026059783a6205f01c5baf93144e7eaefd9
Your Name <you@example.com>
2025-10-29 20:44:55 -0600

    just adding main lol

---
+++ main.py
@@ -0,0 +1,15 @@
+# Chess-Notation visualizer
+# 10/16/2025
+# Zac Abero
+
+# This is a prototype program that visualizes chess notation into short videos, compiled of imagery.
+
+# Structure: File I/O -> Parser -> Interpretation of move -> Token assigned to move -> Image assigned to token
+
+''' Ideas: Weigh eash chess piece akin to their weight in game.
+hey '''
+
+def main():
+
+if __name__ == '__main__':
+  print("running main")
